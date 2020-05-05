c2c0f35ba1ec0df9b2913180d979e8658e2b8938
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-05-05 19:56:40 +0200

    fix: proto file location

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .format(true)
-        .compile(&["../ib_loader/ibbridge.proto"], &["../ib_loader"])?;
+        .compile(&["../ibbridge/ibbridge.proto"], &["../ibbridge"])?;
 
     Ok(())
 }
